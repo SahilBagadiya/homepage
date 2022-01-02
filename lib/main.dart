0740b8ec9ef3ef14ffa568767268d606711300f2
SahilBagadiya <bagadiya.sahil.d@gmail.com>
2022-01-02 20:55:48 +0530

    this is my first commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'myapp',
+      theme: ThemeData(
+        accentColor: Colors.amber,
+        primaryColor: Colors.indigo,
+      ),
+      home: Simple(),
+    );
+  }
+}
+
+class Simple extends StatelessWidget {
+  const Simple({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('home'),
+      ),
+      body: Center(
+        child: Text('this my home page'),
+      ),
+    );
+  }
+}
